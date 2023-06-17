ee396db8c61866c7ca96598dcfd9e611c2f16b39
Temir <galaxy2013545@gmail.com>
2023-06-17 16:41:38 +0600

    Д/з

---
+++ С#work5/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// Console.Write("Ввести число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2){
+//   Console.WriteLine("Третья цифра --> " + anyNumberText[2]);
+// }
+// else {
+//   Console.WriteLine("-> третьей цифры нет(");
+// }
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+// Console.Write("Введи цифру, день недели: ");
+// int dayNum = Convert.ToInt32(Console.ReadLine());
+
+// void CheckDayWeek (int dayNum) {
+//   if (dayNum == 6 || dayNum == 7) {
+//   Console.WriteLine("Выходной -> да");
+//   }
+//   else if (dayNum < 1 || dayNum > 7) {
+//     Console.WriteLine("Не вверный ввод");
+//   }
+//   else Console.WriteLine("Рабочий день -> нет");
+// }
+
+// CheckDayWeek(dayNum);
