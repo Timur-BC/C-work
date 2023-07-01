27822f3ea967f934c34e8bc65cedf7e2ba52d43c
Temir <galaxy2013545@gmail.com>
2023-07-01 12:51:28 +0600

    Д/з 01/07/23

--- C#work2/Program.cs
+++ C#work2/Program.cs
@@ -1,29 +1,87 @@
 ﻿
-int Max(int arg1, int arg2, int arg3) {
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
+// int Max(int arg1, int arg2, int arg3) {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
 
-int a1 = 155555;
-int b1 = 12;
-int c1 = 999;
-int a2 = 77;
-int b2 = 66;
-int c2 = 55;
-int a3 = 44;
-int b3 = 33;
-int c3 = 222;
+// int a1 = 155555;
+// int b1 = 12;
+// int c1 = 999;
+// int a2 = 77;
+// int b2 = 66;
+// int c2 = 55;
+// int a3 = 44;
+// int b3 = 33;
+// int c3 = 222;
 
-// int max1 = Max (a1, b1, c1);
-// int max2 = Max (a2, b2, c2);
-// int max3 = Max (a3, b3, c3);
-// int max = Max (max1, max2, max3);
+// // int max1 = Max (a1, b1, c1);
+// // int max2 = Max (a2, b2, c2);
+// // int max3 = Max (a3, b3, c3);
+// // int max = Max (max1, max2, max3);
 
-int max = Max(
-    Max (a1, b1, c1),
-    Max (a2, b2, c2),
-    Max (a3, b3, c3));
+// int max = Max(
+//     Max (a1, b1, c1),
+//     Max (a2, b2, c2),
+//     Max (a3, b3, c3));
 
-Console.WriteLine(max);
+// Console.WriteLine(max);
+
+
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Clear();
+// Console.WriteLine("Задача 10 ");
+// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+// Console.WriteLine("Ввести трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// Console.Write("Ввести число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2){
+//   Console.WriteLine("Третья цифра --> " + anyNumberText[2]);
+// }
+// else {
+//   Console.WriteLine("-> третьей цифры нет(");
+// }
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+// Console.Write("Введи цифру, день недели: ");
+// int dayNum = Convert.ToInt32(Console.ReadLine());
+
+// void CheckDayWeek (int dayNum) {
+//   if (dayNum == 6 || dayNum == 7) {
+//   Console.WriteLine("Выходной -> да");
+//   }
+//   else if (dayNum < 1 || dayNum > 7) {
+//     Console.WriteLine("Не вверный ввод");
+//   }
+//   else Console.WriteLine("Рабочий день -> нет");
+// }
+
+// CheckDayWeek(dayNum);
+
---
+++ C#work6/Program.cs
@@ -0,0 +1,128 @@
+﻿
+
+// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int[] coordinates = new int[2];
+
+// Console.Write("Введите координату Х: ");
+// coordinates[0] = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y: ");
+// coordinates[1] = Convert.ToInt32(Console.ReadLine());
+
+// if (coordinates[0] > 0 && coordinates[1] > 0)
+// {
+//     System.Console.WriteLine("Точка находится в 1-й четверти");
+// }
+// else if (coordinates[0] < 0 && coordinates[1] > 0)
+// {
+//     System.Console.WriteLine("Точка находится во 2-й четверти");
+// }
+// else if (coordinates[0] < 0 && coordinates[1] < 0)
+// {
+//     System.Console.WriteLine("Точка находится в 3-й четверти");
+// }
+// else if (coordinates[0] > 0 && coordinates[1] < 0)
+// {
+//     System.Console.WriteLine("Точка находится во 4-й четверти");
+// }
+// else
+// {
+//     System.Console.WriteLine("Введены неправильные координаты");
+// }
+
+
+// // Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+// Console.WriteLine("Введите чмсло");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// string[] array = {"X > 0 && Y > 0", "X < 0 && Y > 0", "X > 0 && Y < 0", "X > 0 && Y < 0"};
+
+// if (number == 1)
+// {
+//     Console.WriteLine("X > 0 && Y > 0");
+// }
+
+// else if (number == 2)
+// {
+//     Console.WriteLine("X < 0 && Y > 0");
+// }
+
+// else if (number == 3)
+// {
+//     Console.WriteLine("X > 0 && Y < 0");
+// }
+
+// else if (number == 3)
+// {
+//     Console.WriteLine("X > 0 && Y < 0");
+// }
+
+// else
+// {
+//    Console.WriteLine("Такой четверти не существует"); 
+// }
+
+
+// Math.Pow(3, 4); // Возведение числа 3 в степень 4
+// Math.Sqrt(11); // Взятие квадратного корня
+// Math.Round(3.123123123, 3); // Округление
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// int[] aCoordinate = new int[2];
+//             Console.WriteLine("Введите x первой точки: ");
+//             aCoordinate[0] = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine("Введите y первой точки: ");
+//             aCoordinate[1] = Convert.ToInt32(Console.ReadLine());
+//             int[] bCoordinate = new int[2];
+//             Console.WriteLine("Введите x первой точки: ");
+//             bCoordinate[0] = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine("Введите y первой точки: ");
+//             bCoordinate[1] = Convert.ToInt32(Console.ReadLine());
+
+//             Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((bCoordinate[0] - aCoordinate[0]), 2) + Math.Pow((bCoordinate[1] - aCoordinate[1]),2)),3));
+
+
+
+
+//             Console.WriteLine(string.Join(", ", array));
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int i=1;
+
+// while (i <= number)
+// {
+//     Console.WriteLine(Math.Pow(i , 2)+ " ");
+//     i++;
+// }
+
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i <= number; i++)
+// {
+//     Console.Write(Math.Pow(i, 2) + " ");
+// }
+
+string str = "12321";
+
+if (str [0] == [4] || [1] == [3])
+{
+    System.Console.WhiteLine("Зеркальное");
+}
+else 
+{
+    System.Console.WhiteLine("Нет");
+}
